bb15d8cf05cb278f11b440c462a256aeb9b7d0e3
Toby0727 <hpang05@syr.edu>
2026-01-29 17:41:55 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.title("PDF Reader APP")
 lab1 = st.Page('labs/lab1.py' , title = 'lab 1')
 lab2 = st.Page('labs/lab2.py' , title = 'lab 2')
 pg = st.navigation([lab2, lab1])
-st.set_page_config(page_title= 'IST 488 labs')
+st.set_page_config(page_title= 'PDF READER APP')
 
