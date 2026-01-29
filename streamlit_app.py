24819c5d440d5a2e5b938dbbb44a388587a24677
Toby0727 <hpang05@syr.edu>
2026-01-29 17:40:30 +0000

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-st.title("IST 488 Labs")
+st.title("PDF Reader APP")
 lab1 = st.Page('labs/lab1.py' , title = 'lab 1')
 lab2 = st.Page('labs/lab2.py' , title = 'lab 2')
 pg = st.navigation([lab2, lab1])
 st.set_page_config(page_title= 'IST 488 labs')
+
