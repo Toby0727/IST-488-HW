ae6298056ae2e93af7e4b781a562785cf9a4888d
Toby0727 <hpang05@syr.edu>
2026-02-02 01:45:22 +0000

    install beatifulsoup4

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,16 @@ lab1 = st.Page('labs/lab1.py' , title = 'lab 1')
 lab2 = st.Page('labs/lab2.py' , title = 'lab 2', default=True)
 pg = st.navigation([lab1, lab2])
 st.set_page_config(page_title= 'IST 488 Lab', initial_sidebar_state='expanded')
-pg.run()
+
+import requests
+from bs4 import BeautifulSoup
+
+def read_url_content(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        soup = BeautifulSoup(response.content, 'html.parser')
+        return soup.get_text()
+    except requests.RequestException as e:
+        print(f"Error reading {url}: {e}")
+        return None
