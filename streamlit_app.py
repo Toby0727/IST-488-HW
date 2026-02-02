2ded53047c31e01d7c8fb8254e3df1905a978d04
Toby0727 <hpang05@syr.edu>
2026-02-02 03:08:52 +0000

    pgrun

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ HW 1 = st.Page('HW/HW 1.py' , title = 'HW 1')
 HW 2 = st.Page('HW/HW 2.py' , title = 'HW 2', default=True)
 pg = st.navigation([HW 1, HW 2])
 st.set_page_config(page_title= 'HW Manager', initial_sidebar_state='expanded')
-
+pg.run()
 
