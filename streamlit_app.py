07f87c6b0aee7fbf55355ab466f2a233fa09e43e
Toby0727 <hpang05@syr.edu>
2026-02-02 03:09:44 +0000

    title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-HW 1 = st.Page('HW/HW 1.py' , title = 'HW 1')
-HW 2 = st.Page('HW/HW 2.py' , title = 'HW 2', default=True)
-pg = st.navigation([HW 1, HW 2])
 st.set_page_config(page_title= 'HW Manager', initial_sidebar_state='expanded')
+HW_1 = st.Page('HW/HW 1.py' , title = 'HW 1')
+HW_2 = st.Page('HW/HW 2.py' , title = 'HW 2', default=True)
+pg = st.navigation([HW_1, HW_2])
 pg.run()
 
