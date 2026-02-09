94ee4903e015c116247995cfc97216ea89ec9128
Toby0727 <hpang05@syr.edu>
2026-02-09 02:23:32 +0000

    hw3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 st.set_page_config(page_title= 'HW Manager', initial_sidebar_state='expanded')
 HW_1 = st.Page('HW/HW 1.py' , title = 'HW 1')
-HW_2 = st.Page('HW/HW 2.py' , title = 'HW 2', default=True)
-pg = st.navigation([HW_1, HW_2])
+HW_2 = st.Page('HW/HW 2.py' , title = 'HW 2')
+HW_3 = st.Page('HW/HW 3.py' , title = 'HW 3', default=True)
+pg = st.navigation([HW_1, HW_2, HW_3])
 pg.run()
 
